54ec45578002d66ca0f932bb5a5b15aad649f764
Tom Burns <tom.burns@jadedpixel.com>
2012-07-27 10:49:03 -0400

    Use SecureRandom in place of MD5 for ID generation

--- test/unit/utils_test.rb
+++ test/unit/utils_test.rb
@@ -4,4 +4,10 @@ class UtilsTest < Test::Unit::TestCase
   def test_unique_id_should_be_32_chars_and_alphanumeric
     assert_match /^\w{32}$/, ActiveMerchant::Utils.generate_unique_id
   end
+
+  def test_secure_random_number_generator_available
+    assert_nothing_raised do
+      ActiveMerchant::Utils.generate_unique_id
+    end
+  end
 end
